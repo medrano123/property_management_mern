c49c8ca009f1fbff83afde22dbf6452eb3a69c3a
Giovanni Medrano <giovannimedrano@Giovannis-MBP.hitronhub.home>
2023-03-28 13:14:03 -0500

    backend and frontend

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,36 @@
+import User from '../mongodb/models/user.js';
+
+const getAllUsers = async (req, res) => {
+
+}
+
+const createUsers = async (req, res) => {
+    try {
+        const { name, email, avatar } = req.body;
+    
+        const userExists = await User.findOne({ email });
+    
+        if (userExists) return res.status(200).json(userExists);
+        
+        const newUser = await User.create({
+            name,
+            email,
+            avatar
+        })
+    
+        res.status(200).json(newUser); 
+    } catch (error) {
+        res.status(500).json({ messege: error.messege })
+    }
+
+}
+
+const getUserInfoById = async (req, res) => {
+    
+}
+
+export {  
+    getAllUsers,
+    createUsers,
+    getUserInfoById,
+}
